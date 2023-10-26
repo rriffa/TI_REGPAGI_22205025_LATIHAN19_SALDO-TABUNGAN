b8f3649ca0a91859afba6fda3b435aa454dc09c0
rriffa <145914122+rriffa@users.noreply.github.com>
2023-10-26 14:08:28 +0700

    Update Latihan19.java

--- src/latihan19/Latihan19.java
+++ src/latihan19/Latihan19.java
@@ -18,29 +18,16 @@ public class Latihan19 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Input saldo awal, bunga per bulan, dan saldo target
-        double saldoAwal = 3500000;
-        double bungaBulan = 8;
-        double saldoTarget = 6000000;
-
+        double saldoAwal = 2500000;
+        double bungaBulan = 15;
+        int lamaBulan = 6;
         // Hitung bunga per bulan
         double bungaPerBulan = saldoAwal * bungaBulan / 100;
-
-        // Hitung lama bulan untuk mencapai saldo target
-        int lamaBulan = 1;
+        // Hitung saldo akhir setiap bulan
         double saldoAkhir = saldoAwal;
-        while (saldoAkhir < saldoTarget) {
+        for (int i = 1; i <= lamaBulan; i++) {
             saldoAkhir += bungaPerBulan;
-            lamaBulan++;
-        }
-
-        // Output lama bulan untuk mencapai saldo target
-        System.out.println("Perhitungan lama tabungan sampai mencapai saldo target.");
-        System.out.println("Saldo Awal: Rp." + saldoAwal);
-        System.out.println("Bunga/Bulan (%): " + bungaBulan);
-        System.out.println("Saldo target: Rp." + saldoTarget);
-        System.out.println("Output:");
-        System.out.println("Lama bulan untuk mencapai saldo target: " + lamaBulan);
+            System.out.println("Saldo di bulan ke-" + i + " Rp." + saldoAkhir);
     }
-    
+}
 }
